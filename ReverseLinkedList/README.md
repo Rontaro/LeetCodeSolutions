bb32cc20ddf27c74d00e9d4e812ec0904038f490
Rontaro <antonio.romano-@hotmail.com>
2025-08-19 13:07:24 +0200

    new excercise

---
+++ ReverseLinkedList/README.md
@@ -0,0 +1,17 @@
+# Reverse Linked List
+
+**Problem link:** [LeetCode - Reverse Linked List](https://leetcode.com/problems/reverse-linked-list/submissions/1740739211/?envType=study-plan-v2&envId=leetcode-75)
+
+## Description
+
+Given the head of a singly linked list, reverse the list and return the new head.
+
+### Example
+
+- Input: `head = [1,2,3,4,5]`  
+  Output: `[5,4,3,2,1]`
+
+## Constraints
+
+- The number of nodes in the list is in the range `[0, 5000]`
+- `-5000 <= Node.val <= 5000`
---
+++ ReverseLinkedList/solution.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    let prev = null;
+    let curr = head;
+    if (!curr) {
+        return head;
+    }
+    return ricorsive(prev, curr);
+};
+
+const ricorsive = function(prev, curr) {
+    if (curr) {
+        const next = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = next;
+        return (ricorsive(prev, curr))
+    }
+    return prev;
+}
